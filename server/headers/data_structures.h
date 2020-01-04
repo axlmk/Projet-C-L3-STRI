81d9471646990da92251f4f13dd32eae6cbb5ed9
axel <axel-gauthier@hotmail.fr>
2020-01-04 23:05:37 +0100

    Data structures to manipulate

---
+++ server/headers/data_structures.h
@@ -0,0 +1,31 @@
+#ifndef __DATA_STRUCTURES__
+#define __DATA_STRUCTURES__
+
+#include <string.h>
+
+typedef struct record {
+    char name[15];
+    char firstName[15];
+    char phone[15];
+    char address[30];
+    char email[30];
+    char birthDate[11];
+    char comments[50];
+} record;
+
+typedef struct directory {
+    record recordList[20];
+} directory;
+
+typedef struct account {
+    char username[15];
+    char password[15];
+    directory ownedDirectory;
+    directory sharedDirectory[20];
+} account;
+
+
+
+
+
+#endif

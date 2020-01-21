d3e70420bd9276620a23af4ece4dabb44fca6cb3
axel <axel-gauthier@hotmail.fr>
2020-01-21 19:54:30 +0100

    Addition of the record creation function, not tested

--- server/headers/record.h
+++ server/headers/record.h
@@ -3,6 +3,8 @@
 
 #include "data_structures.h"
 #include "utils.h"
+#include "pdu.h"
+#include "accountio.h"
 
 int writeRecord(char *filename, record r, int cur);
 
--- server/sources/record.c
+++ server/sources/record.c
@@ -47,3 +47,96 @@ int writeRecord(char *filename, record r, int cur) {
     fclose(f);
     return 0;
 }
+
+int readRecord(char *filename, record *a, int cur) {
+    FILE *f = fopen(filename, "rb");
+
+    if(!f)
+        return 1;
+
+    cur *= sizeof(record);
+    fseek(f, cur, SEEK_SET);
+    if(!fread(a, sizeof(record), 1, f)) {
+        fclose(f);
+        return 2;
+    }
+    
+    fclose(f);
+    return 0;
+}
+
+int getSettingsNumber(char *request) {
+    char c;
+    int count = 0, i = 0;
+    while(request[i++] != EOF)
+        if(request[i] == ' ')
+            count++;
+    return count;
+}
+
+void getR_MParameters(char *request, char ***settings) {
+    int i = 0;
+    (*settings)[i++] = strtok(request, " ");
+    while((*settings)[i-1])
+        (*settings)[i++] = strtok(NULL, " ");
+}
+
+//request : user recordIndication field:value [field:value field:value [etc]]
+pdu modifyRecord(char *request) {
+    int n, numRecord;
+    if(!(n = getSettingsNumber(request)))
+        return generateReturnedPdu(KO, "Error from the request.\n");
+    char **settings = malloc(sizeof(char*) * n);
+    getR_MParameters(request, &settings);
+
+    char *path = malloc(sizeof(char) * (strlen(PATH_STORAGE) + strlen(settings[0]) + 1));
+    sprintf(path, "%s%s", PATH_STORAGE, settings[0]);
+    numRecord = strtol(settings[1], NULL, 10);
+
+    record r;
+    char *field;
+    int i = 2;
+    /* a verifier tout ca qu'il y ait pas de magouille de pointeurs/alias */
+    while(i < n) {
+        field = strtok(settings[i++], ":");
+        if(strcmp(field, "name")) {
+            field = strtok(NULL, " ");
+            /*assert rules */
+            strcpy(r.name, field);
+        } else if(strcmp(settings[i], "firstName")) {
+            field = strtok(NULL, " ");
+            /*assert rules */
+            strcpy(r.firstName, field);
+        } else if(strcmp(settings[i], "phone")) {
+            field = strtok(NULL, " ");
+            /*assert rules */
+            strcpy(r.phone, field);
+        } else if(strcmp(settings[i], "address")) {
+            field = strtok(NULL, " ");
+            /*assert rules */
+            strcpy(r.address, field);
+        } else if(strcmp(settings[i], "email")) {
+            field = strtok(NULL, " ");
+            /*assert rules */
+            strcpy(r.email, field);
+        } else if(strcmp(settings[i], "birthDate")) {
+            field = strtok(NULL, " ");
+            /*assert rules */
+            strcpy(r.birthDate, field);
+        } else if(strcmp(settings[i], "comment")) {
+            field = strtok(NULL, " ");
+            /*assert rules */
+            strcpy(r.comments, field);
+        } else {
+            return generateReturnedPdu(KO, "Error from the request. Field not valid\n");
+        }
+    }
+
+    int res;
+    if((res = writeRecord(path, r, numRecord)) == 1)
+        return generateReturnedPdu(KO, "Error from the request. Field not valid\n");
+    else if(res == 2)
+        return generateReturnedPdu(KO, "Error from the request. Field not valid\n");
+    else
+        return generateReturnedPdu(KO, "Error from the request. Field not valid\n");
+}
